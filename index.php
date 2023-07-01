ae4a5e69b6f44ec36a8c24d9ed9b1a6a3d565567
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-07-01 19:40:12 +0700

    fix dropdown user

--- index.php
+++ index.php
@@ -313,7 +313,7 @@ if ($bulan >= 7) {
                 <!-- User Info-->
                 <div class="sidenav-header-inner text-center"><img src="img/Logo.png" alt="person" class="img-fluid rounded-circle">
                     <h2 class="h5">
-                        <?= $nama; ?>
+                        <?= $nama; ?> Tes
                     </h2><span>
                         <?= $level; ?>
                     </span>
@@ -433,11 +433,10 @@ if ($bulan >= 7) {
                         </div>
                         <ul class="nav-menu list-unstyled d-flex flex-md-row align-items-md-center">
                             <!-- Log out-->
-                            <li class="nav-item"><a href="logout.php" class="nav-link logout" onclick="return confirm('Anda yakin ingin keluar?')"> <span class="d-none d-sm-inline-block">Logout</span><i class="fa fa-sign-out"></i></a>
-                            </li>
                             <div class="dropdown">
                                 <button class="btn btn-dark dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
-                                    Dropdown button
+                                    <i class="fa fa-user"></i>
+                                    <?= $nama; ?>
                                 </button>
                                 <div class="dropdown-menu">
                                     <a class="dropdown-item" href="logout.php" onclick="return confirm('Anda yakin ingin keluar?')">
