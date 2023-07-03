274e52b37e0085bfb4b9ea49c4c3aac6a18effe1
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-07-03 21:48:34 +0700

    fix proses input nilai

--- proses_input_nilai.php
+++ proses_input_nilai.php
@@ -17,36 +17,35 @@ $thpel = $_POST['th_pel'];
 $user = $_SESSION['u_abis'];
 $nm = $_POST['nm'];
 
-$cek = mysqli_query($connect,"select * from tbnilai where semester='$semester' and id_siswa='$ids' and username='$user'");
+$cek = mysqli_query($connect, "select * from tbnilai where semester='$semester' and id_siswa='$ids' and username='$user'");
 $jml = mysqli_num_rows($cek);
 
-if ($jml > 0){
-	
+if ($jml > 0) {
 	echo "<script>alert('Nilai Siswa sudah pernah diinput!');
 	document.location='index.php?hal=input_nilai&nm=$nm'</script>";
-	
-} else {	
-$i=0;
-foreach ($an_peng as $nilai1){
-	$query1 = "insert into tbnilai values ('','$apel[$i]','$thpel','$semester','$nilai1','$an_ket[$i]','$an_sikap[$i]','$ids','$user','$tgl')";
-	$simpan1 = mysqli_query($connect, $query1);
-	$i++;
-}
-
-$j=0;
-foreach ($bn_peng as $nilai2){
-	$query2 = "insert into tbnilai values ('','$bpel[$j]','$thpel','$semester','$nilai2','$bn_ket[$j]','$bn_sikap[$j]','$ids','$user','$tgl')";
-	$simpan2 = mysqli_query($connect, $query2);
-	$j++;
-}
-
-if ($simpan1 && $simpan2){
-	echo "<script>alert('Nilai Berhasil Diinput!');
-	document.location='index.php?hal=input_nilai'</script>";
+} else if (!$semester) {
+	echo "<script>alert('semester tidak boleh kosong');
+	document.location='index.php?hal=input_nilai&nm=$nm'</script>";
 } else {
-	echo "<script>alert('Nilai Gagal Diinput!');
-	document.location='index.php?hal=input_nilai'</script>";
-}
+	$i = 0;
+	foreach ($an_peng as $nilai1) {
+		$query1 = "insert into tbnilai values ('','$apel[$i]','$thpel','$semester','$nilai1','$an_ket[$i]','$an_sikap[$i]','$ids','$user','$tgl')";
+		$simpan1 = mysqli_query($connect, $query1);
+		$i++;
+	}
 
+	$j = 0;
+	foreach ($bn_peng as $nilai2) {
+		$query2 = "insert into tbnilai values ('','$bpel[$j]','$thpel','$semester','$nilai2','$bn_ket[$j]','$bn_sikap[$j]','$ids','$user','$tgl')";
+		$simpan2 = mysqli_query($connect, $query2);
+		$j++;
+	}
+
+	if ($simpan1 && $simpan2) {
+		echo "<script>alert('Nilai Berhasil Diinput!');
+	document.location='index.php?hal=input_nilai'</script>";
+	} else {
+		echo "<script>alert('Nilai Gagal Diinput!');
+	document.location='index.php?hal=input_nilai'</script>";
+	}
 }
-?>
