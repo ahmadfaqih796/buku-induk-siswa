e9447b078996a3bfe2fff2f3b07e4e425eba9a25
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-07-05 21:02:40 +0700

    fix pilih siswa

--- pilih_siswa.php
+++ pilih_siswa.php
@@ -1,64 +1,64 @@
 			  <div class="card">
-                <div class="card-header">
-                  <h4><?=getAddress();?></h4>
-                </div>
-                <div class="card-body">
-                  <div class="table-responsive">
-				  <form method="post" action="proses_masuk_kelas.php">
-				  <input type="hidden" value="<?=$_GET['idk'];?>" name="idk">
-                    <table class="table table-striped table-hover table-bordered">
-                      <thead>
-                        <tr>
-                          <th>No</th>
-                          <th>Nama Lengkap</th>
-                          <th>Jenis Kelamin</th>
-                          <th>Tempat Tanggal Lahir</th>
-                          <th width="6%" align="center">Aksi</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-					  <?php
-					  if (isset($_GET['nohal'])){
-						  $nohal = $_GET['nohal'];
-					  } else {
-						  $nohal = 1;
-					  }
-					  $ph = 10;
-					  $start = ($nohal - 1) * $ph;
-					  $no = $start+1;
-					  $la = mysqli_query($connect, "select * from tbsiswa where id_kelas='0' limit $start,$ph");
-					  while ($da = $la->fetch_array(MYSQLI_ASSOC)){
-					  ?>
-                        <tr>
-                          <th scope="row"><?=$no;?></th>
-                          <td><?=$da['nm_lengk_siswa'];?></td>
-                          <td><?=$da['jenis_kelamin'];?></td>
-                          <td><?=$da['tmpt_lhr_siswa'];?>, <?=$da['tgl_lhr_siswa'];?></td>
-                          <td>
-							<div class="i-checks">
-								<input id="<?=$da['id_siswa'];?>" type="checkbox" name="ids[]" value="<?=$da['id_siswa'];?>" class="form-control-custom">
-								<label for="<?=$da['id_siswa'];?>">&nbsp;&nbsp;</label>
-							</div>
-						  </td>
-                        </tr>
-					  <?php
-					  $no++;
-					  }
-					  $lb = mysqli_query($connect, "select * from tbsiswa where id_kelas='0'");
-					  $jd = mysqli_num_rows($lb);
-					  $jumhal = ceil($jd/$ph);
-					  ?>
-					  <tr>
-					  <td colspan="4"></td>
-					  <td><input type="submit" value="Pilih" class="btn btn-primary"></td>
-					  </tr>
-                      </tbody>
-                    </table>
-					</form>
-                  </div>
-				  <?php
-				  include "pagination.php";
-				  pagination($jumhal,$nohal,"pilih_siswa");
-				  ?>
-                </div>
-              </div>
+			  	<div class="card-header">
+			  		<h4><?= getAddress(); ?></h4>
+			  	</div>
+			  	<div class="card-body">
+			  		<div class="table-responsive">
+			  			<form method="post" action="proses_masuk_kelas.php">
+			  				<input type="hidden" value="<?= $_GET['idk']; ?>" name="idk">
+			  				<table class="table table-striped table-hover table-bordered">
+			  					<thead>
+			  						<tr>
+			  							<th>No</th>
+			  							<th>Nama Lengkap</th>
+			  							<th>Jenis Kelamin</th>
+			  							<th>Tempat Tanggal Lahir</th>
+			  							<th width="6%" align="center">Aksi</th>
+			  						</tr>
+			  					</thead>
+			  					<tbody>
+			  						<?php
+										if (isset($_GET['nohal'])) {
+											$nohal = $_GET['nohal'];
+										} else {
+											$nohal = 1;
+										}
+										$ph = 10;
+										$start = ($nohal - 1) * $ph;
+										$no = $start + 1;
+										$la = mysqli_query($connect, "select * from tbsiswa where id_kelas='0' limit $start,$ph");
+										while ($da = $la->fetch_array(MYSQLI_ASSOC)) {
+										?>
+			  							<tr>
+			  								<th scope="row"><?= $no; ?></th>
+			  								<td><?= $da['nm_lengk_siswa']; ?></td>
+			  								<td><?= $da['jenis_kelamin']; ?></td>
+			  								<td><?= $da['tmpt_lhr_siswa']; ?>, <?= $da['tgl_lhr_siswa']; ?></td>
+			  								<td>
+			  									<div class="i-checks">
+			  										<input id="<?= $da['id_siswa']; ?>" type="checkbox" name="ids[]" value="<?= $da['id_siswa']; ?>" class="form-control-custom">
+			  										<label for="<?= $da['id_siswa']; ?>">&nbsp;&nbsp;</label>
+			  									</div>
+			  								</td>
+			  							</tr>
+			  						<?php
+											$no++;
+										}
+										$lb = mysqli_query($connect, "select * from tbsiswa where id_kelas='0'");
+										$jd = mysqli_num_rows($lb);
+										$jumhal = ceil($jd / $ph);
+										?>
+			  						<tr>
+			  							<td colspan="4"></td>
+			  							<td><input type="submit" value="Pilih" class="btn btn-primary"></td>
+			  						</tr>
+			  					</tbody>
+			  				</table>
+			  			</form>
+			  		</div>
+			  		<?php
+						include "pagination.php";
+						pagination($jumhal, $nohal, "pilih_siswa");
+						?>
+			  	</div>
+			  </div>
--- proses_masuk_kelas.php
+++ proses_masuk_kelas.php
@@ -1,23 +1,23 @@
 <?php
 include "koneksi.php";
 error_reporting(0);
-if(isset($_POST["idk"]) && isset($_POST["ids"])){
+if (isset($_POST["idk"]) && isset($_POST["ids"])) {
 	$idk = $_POST['idk'];
+	echo $idk;
 	$ids = $_POST['ids'];
-	$lb = mysqli_query($connect, "select * from tbsiswa where idk=$idk ");
+	$lb = mysqli_query($connect, "select * from tbsiswa where id_kelas=$idk");
 	$jd = mysqli_num_rows($lb);
 
 	if ($jd >= 20) {
-	echo"<script>alert('Kelas Penuh')
+		echo "<script>alert('Kelas Penuh')
 		document.location='index.php?hal=pilih_siswa'</script>";
-	}else{
-	foreach ($ids as $id){
-		mysqli_query($connect,"update tbsiswa set id_kelas = '$idk' where id_siswa = '$id'");
+	} else {
+		foreach ($ids as $id) {
+			mysqli_query($connect, "update tbsiswa set id_kelas = '$idk' where id_siswa = '$id'");
+		}
+		header("location:index.php?hal=masuk_kelas&idk=" . $idk);
 	}
-	header("location:index.php?hal=masuk_kelas&idk=".$idk);
-	}
-}else{
-	echo"<script>alert('Mohon maaf, anda belum memilih siswa !')
+} else {
+	echo "<script>alert('Mohon maaf, anda belum memilih siswa !')
 		document.location='index.php?hal=pilih_siswa'</script>";
 }
-?>
